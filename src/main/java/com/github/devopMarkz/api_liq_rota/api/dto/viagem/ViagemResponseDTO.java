0aad01c93aa6f13c3045e8aa8a988bf50587d6de
devopMarkz <marcosdev2002@gmail.com>
2025-08-30 17:53:38 -0300

    Criando DTO de viagem

---
+++ src/main/java/com/github/devopMarkz/api_liq_rota/api/dto/viagem/ViagemResponseDTO.java
@@ -0,0 +1,34 @@
+package com.github.devopMarkz.api_liq_rota.api.dto.viagem;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.devopMarkz.api_liq_rota.api.dto.usuario.UsuarioResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViagemResponseDTO {
+    private Long id;
+
+    private UsuarioResponseDTO usuario;
+
+    private String origem;
+    private String destino;
+
+    private BigDecimal distanciaKm;
+    private BigDecimal consumoKmPorLitro;
+    private BigDecimal precoLitro;
+    private BigDecimal gastosAdicionais;
+    private BigDecimal valorFrete;
+    private Boolean idaEVolta;
+
+    private BigDecimal custoCombustivel;
+    private BigDecimal valorLiquido;
+
+    private OffsetDateTime createdAt;
+}
