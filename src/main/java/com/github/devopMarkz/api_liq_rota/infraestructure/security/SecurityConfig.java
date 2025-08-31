7a959360c900b8abc3638714933957f02f93df10
devopMarkz <marcosdev2002@gmail.com>
2025-08-31 02:15:34 -0300

    Configurando CORS

--- src/main/java/com/github/devopMarkz/api_liq_rota/infraestructure/security/SecurityConfig.java
+++ src/main/java/com/github/devopMarkz/api_liq_rota/infraestructure/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081"));
+        configuration.setAllowedOrigins(List.of("https://liq-frota.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
